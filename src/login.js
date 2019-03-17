58897d54545412ac03e035f0ac9741917117e462
JS Learning <damnedthinker@gmail.com>
2019-03-17 20:55:10 +0545

    1st commit: authentication done

---
+++ src/login.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import fire from './config/fire';
+
+class Login extends Component{
+    constructor(props){
+        super(props);
+        this.login = this.login.bind(this);
+        this.handleChange =this.handleChange.bind(this);
+        this.signup = this.signup.bind(this);
+        this.state={
+            email :'',
+            password :''
+          
+        }
+        
+    }
+    
+    login(e) {
+        e.preventDefault();
+        fire.auth().signInWithEmailAndPassword( this.state.email, this.state.password ).catch( (error)=> {console.log(error);} );
+    }
+    
+    signup(e){
+        e.preventDefault();
+        fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch( function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+            console.log(error.message);
+  // ...
+});
+    }
+    
+    handleChange(e){
+        this.setState({ [e.target.name]: e.target.value});
+        
+    }
+    
+    render()
+    {
+        
+        return(
+        <div className="col-md-6">
+            <form>
+                <div class="form-group">
+                <label for="InputEmail1"> Email Address</label>
+                <input value={this.state.email} onChange ={this.handleChange} type="email" name="email"
+                class="form-control" id="InputEmail1" aria-describedby ="emailHelp"
+                placeholder="Enter email" />
+                <small id ="emailHelp" class="form-text text-muted"> We will never share your email with anyone else .</small>
+                </div>
+            
+                <div class ="form-group">
+                    <label for ="InputPassword1">Password</label>
+                    <input value={this.state.password} onChange={this.handleChange} type='password'
+            name='password' class="form-control" id="InputPassword1" 
+                placeholder="Enter password" />
+                </div>    
+            
+            
+                <button type="submit" onClick={this.login} class="btn btn-primary"> Login </button>
+                <button onClick={this.signup} style={ {marginLeft: '25px'}} className='btn btn-success'> Signup</button>
+            
+            </form>
+            
+        </div>
+            );
+    }
+}
+export default Login;
+        
+        
+        
+        
+        
+        
+    
+    
+
+
