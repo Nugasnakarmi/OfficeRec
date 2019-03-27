251554fd6493c9d9176b3eb0516ec98eb5f1f1a6
PurakHero <PurakHero>
2019-03-27 19:55:43 +0545

    moved fixDate to a separate component

--- src/AdminWindow.js
+++ src/AdminWindow.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import fire from './config/fire'
+import fire from './config/fire';
+import fixDate from './FixDate';
 
 class AdminWindow extends Component {
     constructor(props) {
@@ -11,10 +12,7 @@ class AdminWindow extends Component {
         };
 
         this.db = fire.firestore();
-        // this.storedData ='';
-
         this.getUserDetails = this.getUserDetails.bind(this);
-        this.fixDate= this.fixDate.bind(this);
         this.handleClick = this.handleClick.bind(this);
     }
     getUserDetails(mail) {
@@ -26,106 +24,61 @@ class AdminWindow extends Component {
                         searched: true
                     }
                     )
-                    // localStorage.setItem('localInfo', JSON.stringify(doc.data()));
-                    // this.storedData = JSON.parse(localStorage.getItem('localInfo'));
-
-
-
                 }).catch(function (error) {
                     console.log(error.message);
                     window.alert("user not found")
                 }
 
                 )
-           
+
         }
     }
     handleChange = e => this.setState({ [e.target.name]: e.target.value });
-    
+
     handleClick() {
-        (this.state.email !== '') ? (this.getUserDetails(this.state.email)) : ( window.alert("enter email please") );
+        (this.state.email !== '') ? (this.getUserDetails(this.state.email)) : (window.alert("enter email please"));
     }
-    fixDate( date ){
-        if (date) {
-                    
-            var currentDate;
-            
-            var dateHolder = date.toDate();
-            currentDate = dateHolder;
-            var day = currentDate.getDate();
-            var month = currentDate.getMonth(); //Be careful! January is 0 not 1
-            var year = currentDate.getFullYear();
-            // function pad(n) {
-            //     return n<10 ? '0'+n : n;
-            // }
-            function ordinal(i) {
-                var j = i % 10,
-                    k = i % 100;
-                if (j == 1 && k != 11) {
-                    return i + "st";
-                }
-                if (j == 2 && k != 12) {
-                    return i + "nd";
-                }
-                if (j == 3 && k != 13) {
-                    return i + "rd";
-                }
-                return i + "th";
-            }
-            // var mmddyyyy = pad(month + 1) + "/" + pad(day) + "/" + year;
-            var monthNames = [
-                "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-              ];
-              
-            //   var dateWithFullMonthName = monthNames[month] + " " + pad(date) + ", " + year;
-              var ordinalDate = ordinal(day) + " " + monthNames[month] + ", " + year;
-            return ordinalDate;
-        }
-    }
-    
+
     render() {
         var date = this.state.userInfo['Date of Birth'];
-       
-
-        
-        return (  
-            <div className ='admin-panel' >
+       return (
+            <div className='admin-panel' >
                 <h1>Admin Panel</h1>
                 <p>Search and view User Records here</p>
-                
-                <form >
-                
-                <div className="form-group">
-                    <input value ={ this.state.email } name ="email" type="email" onChange={this.handleChange}
-                    className="form-control" id="Inputname1" placeholder="Enter email" />
-                    <label htmlFor ="Inputemail1"><small>email of user </small> </label>
-                </div> 
-                
-                    
-                </form>
-             
 
-             <button  onClick ={this.handleClick} className="btn btn-primary">Search</button> 
-              
-         {  this.state.searched ? ( <div className="precords-content"> 
-                <p className='content-para'>User Email: {this.state.email }</p>
-                {console.log("displaying")}
-                <p className='content-para'>Name: {this.state.userInfo['Name']}</p>
-                <p className='content-para'>Citizenship Number: { this.state.userInfo['Citizenship Number']}</p>
-                <p className='content-para'>Date of Birth: {date ? this.fixDate(date) : 'Not Available'}</p>
+                <form >
+
+                    <div className="form-group">
+                        <input value={this.state.email} name="email" type="email" onChange={this.handleChange}
+                            className="form-control" id="Inputname1" placeholder="Enter email" />
+                        <label htmlFor="Inputemail1"><small>email of user </small> </label>
+                    </div>
+
+
+                </form>
+
+
+                <button onClick={this.handleClick} className="btn btn-primary">Search</button>
+
+                {this.state.searched ? (<div className="precords-content">
+                    <p className='content-para'>User Email: {this.state.email}</p>
+                    {console.log("displaying")}
+                    <p className='content-para'>Name: {this.state.userInfo['Name']}</p>
+                    <p className='content-para'>Citizenship Number: {this.state.userInfo['Citizenship Number']}</p>
+                    <p className='content-para'>Date of Birth: {date ? fixDate(date) : 'Not Available'}</p>
                 </div>
                 )
-    : 
-    (
-            <div> 
-                <small> <i> Please search for user</i></small> 
-            </div> 
-    )
-         }
-         
+                    :
+                    (
+                        <div>
+                            <small> <i> Please search for user</i></small>
+                        </div>
+                    )
+                }
+
             </div>
         );
     }
 }
- 
+
 export default AdminWindow;
--- src/MainWindow.js
+++ src/MainWindow.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import fire from './config/fire';
 import './mainwindow.css';
-
+import fixDate from './FixDate';
 
 class MainWindow extends Component {
     constructor(props) {
         super(props);
         this.state = { userInfo: '' };
         this.db = fire.firestore();
-        this.fixDate = this.fixDate.bind(this);
         var storage = fire.storage();
         const profilePhotoPath = storage.ref(this.props.user + '/profilepic.jpg');
         profilePhotoPath.getDownloadURL().then((url) => {
@@ -34,75 +33,23 @@ class MainWindow extends Component {
                 };
             });
         }
-        // this.authListener = this.authListener.bind(this);
     }
-    fixDate( date ){
-    if (date) {
-                
-        var currentDate;
-        
-        var dateHolder = date.toDate();
-        currentDate = dateHolder;
-        var day = currentDate.getDate();
-        var month = currentDate.getMonth(); //Be careful! January is 0 not 1
-        var year = currentDate.getFullYear();
-        // function pad(n) {
-        //     return n<10 ? '0'+n : n;
-        // }
-        function ordinal(i) {
-            var j = i % 10,
-                k = i % 100;
-            if (j == 1 && k != 11) {
-                return i + "st";
-            }
-            if (j == 2 && k != 12) {
-                return i + "nd";
-            }
-            if (j == 3 && k != 13) {
-                return i + "rd";
-            }
-            return i + "th";
-        }
-        // var mmddyyyy = pad(month + 1) + "/" + pad(day) + "/" + year;
-        var monthNames = [
-            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-          ];
-          
-        //   var dateWithFullMonthName = monthNames[month] + " " + pad(date) + ", " + year;
-          var ordinalDate = ordinal(day) + " " + monthNames[month] + ", " + year;
-        return ordinalDate;
-    }
-}
-
-    // componentWillMount(){
-    //     var db = fire.firestore();
-    //     var datab = db.collection('User');
-    //     console.log (datab);
-    // }
 
     render() {
         console.log("StoredData inside render", this.storedData);
-        //console.log("Date of birth", this.state.userInfo['Date of Birth']['seconds']);
-        //var link = this.props.link;
         var date = this.state.userInfo['Date of Birth'];
-        var dateHolder = null;
-        //console.log ('dATE: ', date);
-        
-
-        
-        //console.log("After if: ", dateHolder);
         return (
             <div>
                 {/* <nav className='navbar-main'></nav> */}
                 <h1 className='mainwindow-header'>Dashboard</h1>
                 <div className='dashboard-content'>
                     <picture>
-                        <img src={this.state.imageUrl} className="img-fluid img-thumbnail rounded float-right" alt="Profile Picture" width = '200' height = '200'></img>
+                        <img src={this.state.imageUrl} className="img-fluid img-thumbnail rounded float-right" alt="Profile Picture" width='200' height='200'></img>
                     </picture>
                     <p className='content-para'>User ID: {this.props.user}</p>
                     <p className='content-para'>Name: {this.storedData ? this.storedData['Name'] : this.state.userInfo['Name']}</p>
                     <p className='content-para'>Citizenship Number: {this.storedData ? this.storedData['Citizenship Number'] : this.state.userInfo['Citizenship Number']}</p>
-                    <p className='content-para'>Date of Birth: {date ? this.fixDate(date) : 'Not Available'}</p>
+                    <p className='content-para'>Date of Birth: {date ? fixDate(date) : 'Not Available'}</p>
                 </div>
             </div>
         );
