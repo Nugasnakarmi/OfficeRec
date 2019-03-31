a543c11050f030f288311c080bed68eb5e86fd47
PurakAdh <paripurak@gmail.com>
2019-03-31 17:57:03 +0545

    Updates

--- precache-manifest.1161ea870013ba60e6f001f055c5f47d.js
+++ precache-manifest.1161ea870013ba60e6f001f055c5f47d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/OfficeRec/static/js/runtime~main.f8870734.js"
   },
   {
-    "revision": "2f94febd5c148f1a3141",
-    "url": "/OfficeRec/static/js/main.bcbb95fe.chunk.js"
+    "revision": "214637354b04487a912c",
+    "url": "/OfficeRec/static/js/main.f3ab63ba.chunk.js"
   },
   {
     "revision": "2321686f11af7117793b",
     "url": "/OfficeRec/static/js/2.23225e15.chunk.js"
   },
   {
-    "revision": "2f94febd5c148f1a3141",
+    "revision": "214637354b04487a912c",
     "url": "/OfficeRec/static/css/main.896359de.chunk.css"
   },
   {
-    "revision": "893d301ed8facd107f17d46fa477b984",
+    "revision": "d480f78e51f54f585f31a8f052f4591d",
     "url": "/OfficeRec/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/OfficeRec/precache-manifest.4661c0d135cdea2eb50adbcfe965bb43.js"
+  "/OfficeRec/precache-manifest.1161ea870013ba60e6f001f055c5f47d.js"
 );
 
 workbox.clientsClaim();
