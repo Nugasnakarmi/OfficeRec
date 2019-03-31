180c5260179640a29c82de633c3ff0e3d7e08d29
PurakAdh <paripurak@gmail.com>
2019-03-31 11:40:42 +0545

    Updates

---
+++ precache-manifest.2c350b638f162115029f2c18fc63eb8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bb2ce202ee48160982c",
+    "url": "/OfficeRec/static/js/runtime~main.f8870734.js"
+  },
+  {
+    "revision": "2a777764af97f2c31802",
+    "url": "/OfficeRec/static/js/main.7760349b.chunk.js"
+  },
+  {
+    "revision": "752b06f1d0d441c54f10",
+    "url": "/OfficeRec/static/js/2.0874c097.chunk.js"
+  },
+  {
+    "revision": "2a777764af97f2c31802",
+    "url": "/OfficeRec/static/css/main.906d2cad.chunk.css"
+  },
+  {
+    "revision": "39ec58f22bb25756cfd2ed0fdb7849e8",
+    "url": "/OfficeRec/index.html"
+  }
+];
