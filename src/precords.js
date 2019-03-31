b4edd5cf8d7363856011bacae523962f050491d6
Nugasnakarmi <damnedthinker@gmail.com>
2019-03-31 17:23:06 +0545

    home details added

--- src/precords.js
+++ src/precords.js
@@ -18,6 +18,7 @@ class Precords extends Component {
         this.writeVehicleDetails = this.writeVehicleDetails.bind(this);
         this.writeLandDetails = this.writeLandDetails.bind(this);
         this.writeIncomeDetails = this.writeIncomeDetails.bind(this);
+        this.writeHouseDetails= this.writeHouseDetails.bind(this);
     }
 
     handleChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -189,6 +190,44 @@ class Precords extends Component {
         }
     }
 
+    writeHouseDetails(e){
+        e.preventDefault();
+        var userRef;
+        var houseRef;
+        var droplist = document.getElementById("drop-house")
+            var selected = droplist.options[droplist.selectedIndex].value;
+            console.log(selected);
+        if (this.state.email) {
+            userRef = this.db.collection("UserBase").doc(this.state.email).get().then((doc) => {
+                if (doc.data()) {
+                    houseRef = this.db.collection("UserBase").doc(this.state.email).collection("house-tax");
+                    
+                    houseRef.doc(this.state.houseno).set({
+                        houseno : parseFloat(this.state.houseno),
+                        Location: {
+                            province: this.state.hprovince,
+                            district: this.state.hdistrict,
+                            municipality: this.state.hmunicipality,
+                            ward: parseFloat(this.state.hward)
+
+                        },
+                        nostoreys: parseFloat( this.state.storey),
+                        type : selected,
+                        ['due date']: firebase.firestore.Timestamp.fromDate(new Date(this.state.dueDateHouse)),
+                        taxAmount : parseFloat(this.state.taxAmountHouse)
+
+                    }).then(() => { window.alert("updated successfully") }).catch((error) => { window.alert(error.message) });
+                }
+                else {
+                    window.alert("User does not exist")
+                }
+            })
+        }
+        else {
+            window.alert("User cannot be empty");
+        }
+                    
+    }
     writeIncomeDetails(e) {
         e.preventDefault();
         var userRef;
@@ -199,12 +238,12 @@ class Precords extends Component {
                     incomeRef = this.db.collection("UserBase").doc(this.state.email).collection("income-tax");
 
                     incomeRef.doc(this.state.bn).set({
-                        PAN:   this.state.PAN,
+                        PAN:   parseFloat(this.state.PAN),
                         ['business name']: this.state.bn,
                         ['type of employment']: this.state.employType,
-                        ['annual income']: this.state.income,
-                        taxAmount: this.state.taxAmountIncome,
-                        ['due date']:this.state.dueDateIncome
+                        ['annual income']: parseFloat(this.state.income),
+                        taxAmount: parseFloat(this.state.taxAmountIncome),
+                        ['due date']:firebase.firestore.Timestamp.fromDate(new Date(this.state.dueDateIncome))
 
                     }).then(() => { window.alert("updated successfully") }).catch((error) => { window.alert(error.message) });
                 }
@@ -323,6 +362,61 @@ class Precords extends Component {
                             <button onClick={this.writeLandDetails} className="btn btn-primary">Submit</button>
                         </div>
                     </section>
+                    <section>
+                        <h2> House details </h2>
+                        <div className="form-row">
+                            <div className="col-md-12 mb-3">
+                                <label htmlFor="houseno">House number</label>
+                                <input value={this.state.houseno} id="houseno" name="houseno" type ="number" className="form-control" onChange={this.handleChange} placeholder="Eg: house number"></input>
+                           
+                           </div>
+                           
+                        </div>
+                        <label htmlFor="inputHouseLocation">Enter Location</label>
+                        <div class="form-row" id="inputHouseLocation">
+                            <div class="col-md-3 mb-3">
+                                <input value={this.state.hprovince} id="inputhprovince" name="hprovince" className="form-control" type="text" onChange={this.handleChange} placeholder=" Province"></input>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <input value={this.state.hdistrict} id="inputhdistrict" name="hdistrict" className="form-control" type="text" onChange={this.handleChange} placeholder=" District "></input>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <input value={this.state.hmunicipality} id="inputhmuni" name="hmunicipality" className="form-control" type="text" onChange={this.handleChange} placeholder=" Municipality"></input>
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <input value={this.state.hward} id="inputhward" name="hward" type="number" className="form-control" min="1" onChange={this.handleChange} placeholder=" Ward"></input>
+                            </div>
+                        </div>
+                        <div className="form-row">
+                            
+                            <div class="col-md-6 mb-3">
+                                <label htmlFor="storey">Number of Storeys</label>
+                                <input value={this.state.storey} id="storey" name="storey" className="form-control" onChange={this.handleChange} placeholder="Eg: 4"></input>                            </div>
+
+                                <div class="col-md-6 mb-3">  
+                                <label htmlFor="drop-house">Type of residence</label>
+                                <select id="drop-house" className="custom-select">
+                                <option>Residential</option>
+                                <option>Rented</option>
+                            </select> 
+                                </div> 
+                        
+                        </div>
+
+                        <div className="form-row">
+                            <div class="col-md-6 mb-3">
+                                <label htmlFor="inputDate" position="left">Due date</label>
+                                <input value={this.state.dueDateHouse} className="form-control" id="inputDateHouse" name="dueDateHouse" type="date" onChange={this.handleChange} placeholder="Eg: 12th March 2019"></input>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label htmlFor="inputTax" position="left">Tax amount</label>
+                                <input value={this.state.taxAmountHouse} className="form-control" id="inputTaxHouse" name="taxAmountHouse" type="number" min="0" onChange={this.handleChange} placeholder="Rs 5000"></input>
+
+                            </div>
+                            <button onClick={this.writeHouseDetails} className="btn btn-primary">Submit</button>
+                        </div>
+
+                    </section>
 
                     <section>
                         <h2> Income details </h2>
@@ -337,7 +431,7 @@ class Precords extends Component {
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label htmlFor="employType"> Employment type</label>
-                                <input value={this.state.employType} id="employType" name="employType" className="form-control" onChange={this.handleChange} placeholder="Eg: Business"></input>
+                                <input value={this.state.employType} id="employType" name="employType" className="form-control" onChange={this.handleChange} placeholder="Eg: Business-person"></input>
                             </div>
                         </div>
                         <div className="form-row">
