f0bbebe2b884cd2e6d3b2a9512d48045acc218d7
PurakHero <PurakHero>
2019-03-31 10:51:31 +0545

    abc

--- src/Land.js
+++ src/Land.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './detailscontent.css';
+import fixDate from './FixDate';
 
 class Land extends Component {
     constructor(props) {
@@ -20,17 +21,20 @@ class Land extends Component {
 
     render() {
         console.log("RENDER");
-        return (<div className = "item-box">
+        return (<div className="item-box">
             <h3>Land Record</h3>
             <div>
-                <p className='content-para'>Location</p>
-                <p className='content-para'>Province: {this.props.details.Location.province}</p>
-                <p className='content-para'>District: {this.props.details.Location.district}</p>
-                <p className='content-para'>Municipality: {this.props.details.Location.municipality}</p>
-                <p className='content-para'>Ward Number: {this.props.details.Location.ward}</p>
-                <p className='content-para'>Listing ID: {this.props.details.listingID}</p>
-                <p className='content-para'>Tax Amount: {this.props.details.taxAmount}</p>
-                <p className='content-para'>Due: {this.props.details['due-date']}</p>
+                <div className="location">
+                    <p className='content-para'><b>Location</b></p>
+                    <p className='content-para'>Province: {this.props.details.Location.province}</p>
+                    <p className='content-para'>District: {this.props.details.Location.district}</p>
+                    <p className='content-para'>Municipality: {this.props.details.Location.municipality}</p>
+                    <p className='content-para'>Ward Number: {this.props.details.Location.ward}</p>
+                </div>
+                <p className='content-para'>Area: {/*this.props.details.listingId*/}</p>
+                <p className='content-para'>Listing ID: {this.props.details.listingId}</p>
+                <p className='content-para'>Tax Amount: Rs. {this.props.details.taxAmount}</p>
+                <p className='content-para'>Due: {fixDate(this.props.details['due date'])}</p>
             </div>
         </div>);
     }
